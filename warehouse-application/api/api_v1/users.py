ed94976cdd8c5b4cc9b269f3d7569dc3150aa516
Your New Username <yournewemail@example.com>
2024-09-27 06:30:52 +0500

    fixes-for-users-controller

--- warehouse-application/api/api_v1/users.py
+++ warehouse-application/api/api_v1/users.py
@@ -4,14 +4,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
 from core.schemas.user import UserRead, UserCreate
-from crud.users import get_all_users
+from crud import users as users_crud
 
 router = APIRouter(tags=["Users"])
 
 
 @router.get("", response_model=list[UserRead])
-async def get_users(session: AsyncSession = Depends(db_helper.session_getter)):
-    users = await get_all_users(session=session)
+async def get_all_users(session: AsyncSession = Depends(db_helper.session_getter)):
+    users = await users_crud.get_all_users(session=session)
     return users
 
 
@@ -19,5 +19,5 @@ async def get_users(session: AsyncSession = Depends(db_helper.session_getter)):
 async def create_user(
     user: UserCreate, session: AsyncSession = Depends(db_helper.session_getter)
 ):
-    user = await create_user(session=session, user=user)
+    user = await users_crud.create_user(session=session, user=user)
     return user
